e97b462cc31abef96fb751a4171de3bae16362ae
spaceecs <azaza2046goi@gmail.com>
2025-06-03 09:37:59 +0300

    Created a Next.js project and added files for further work

---
+++ src/components/TabsContainer.tsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+interface Tab {
+    id: string;
+    title: string;
+    icon: string;
+    url: string;
+    pinned: boolean;
+}
+
+export default function TabsContainer() {
+
+}
